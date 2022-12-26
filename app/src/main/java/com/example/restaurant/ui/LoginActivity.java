4f2b64e40f293ffadea1398323c485c785f00911
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-26 10:00:42 -0800

    Implement Login Page UI

--- app/src/main/java/com/example/restaurant/ui/LoginActivity.java
+++ app/src/main/java/com/example/restaurant/ui/LoginActivity.java
@@ -1,13 +1,67 @@
 package com.example.restaurant.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.restaurant.R;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEtUsername;
+    private EditText mEtPassword;
+    private Button mBtnLogin;
+    private TextView mBtnRegister;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+        initEvent();
     }
+
+    private void initEvent() {
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo
+                toOrderActivity();
+            }
+        });
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toRegisterActivity();
+            }
+        });
+    }
+
+    private void initView() {
+        mEtUsername = (EditText) findViewById(R.id.id_et_username);
+        mEtPassword = (EditText) findViewById(R.id.id_et_password);
+        mBtnLogin = (Button) findViewById(R.id.id_btn_login);
+        mBtnRegister = (TextView) findViewById(R.id.id_btn_register);
+    }
+
+    private void toOrderActivity() {
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toRegisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
