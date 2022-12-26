2d26c0e90e4711aacb628f90553b3dcb29ea1770
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-26 10:47:14 -0800

    Implement Register Page UI

---
+++ app/src/main/java/com/example/restaurant/ui/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.example.restaurant.ui;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.restaurant.R;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(0xFF000000);
+        }
+
+    }
+
+    protected void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
+
